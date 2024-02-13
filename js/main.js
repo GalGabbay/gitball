2af36a4d516c3fec4ebaab10fc154b9bfc1a8dbf
GalGabbay <gabbaygal@gmail.com>
2024-02-13 19:37:35 +0200

    Add a 1 second transition to make the ball change size smoothly

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 gBallSize = 100
 gBallIncrement = 50
 
+
+
 function onBallClick() {
 
     var elBall = document.querySelector('.ball')
     
+    if (gBallSize === 400) gBallSize = 50
+
+
     gBallSize += gBallIncrement
 
     elBall.style.height = gBallSize + 'px'
     elBall.style.width = gBallSize + 'px'
     elBall.innerText = `${gBallSize}`
+
+
+
 
 }
 
