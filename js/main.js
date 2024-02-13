0f58639bc7ac71b9ffdba431cf8ac954c5c416f8
GalGabbay <gabbaygal@gmail.com>
2024-02-13 19:18:12 +0200

    Increment the ball’s width & height by 50px and display the updated diameter inside the ball

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-
+gBallSize = 100
+gBallIncrement = 50
 
 function onBallClick() {
-console.log('Hello')
+
+    var elBall = document.querySelector('.ball')
+    
+    gBallSize += gBallIncrement
+
+    elBall.style.height = gBallSize + 'px'
+    elBall.style.width = gBallSize + 'px'
+    elBall.innerText = `${gBallSize}`
 
 }
+
+
+
